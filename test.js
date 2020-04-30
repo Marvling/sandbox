b7d9532442bb7c5d8d12527b293103623cb0d057
Marvling <33942399+Marvling@users.noreply.github.com>
2020-04-29 18:37:05 -0700

    kumda oynadım

--- test.js
+++ test.js
@@ -1,6 +1,26 @@
+"use strict";
+
 function greetings(){
     return 's.a kanka';
 }
 
 let message = greetings();
 console.log(message);
+
+// alert('hello canım');
+
+let admin;
+let name = 'John';
+
+admin = name;
+console.log(admin);
+
+
+
+let userName = prompt('name?', defaultStatus);
+let result = confirm(`is your name ${userName}?`);
+
+    if (result === true) {
+        alert(`${userName}`);
+}
+
