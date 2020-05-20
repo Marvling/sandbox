92ba6a25adae3270eea96c6b0b937c9a77f39f43
Marvling <33942399+Marvling@users.noreply.github.com>
2020-05-19 18:43:54 -0700

    played around with p5 noises

--- p5/noise1.js
+++ p5/noise1.js
@@ -1,39 +1,26 @@
-var xOff1 = 0;
-var xOff2 = 100;
-var xOff3 = 0;
+let xOff1 = 0
+let xOff2 = 100
 
-var nStart = 10;
-var increment = 0.01;
+let y = 0;
 
 function setup() {
-    createCanvas(1000, 800);
-    background(100);
-    frameRate(60);
+    createCanvas(600, 600);
+
+    background(255);
+
 }
 
 function draw() {
-    background(80);
-    stroke(255);
-    noFill();
 
+    stroke(0);
     beginShape();
-    var xoff = nStart;
-    for (var x = 0; x < width; x++) {
+    let x = noise(xOff1) * width;
+    y++;
+    vertex(x, y);
 
-        stroke(255);
-        var y = noise(xoff) * height;
-        vertex(x, y);
-        xoff += increment
-    }
-    endShape();
 
-    nStart += increment
-    // noLoop();
+    // let y = noise(xOff2) * height;
 
-    // var x = map(noise(xOff1), 0, 1, 0, width)
-    // var y = map(noise(xOff2), 0, 1, 0, height)
-
-    // ellipse(x,y, 40, 40);
-    // xOff1 += 0.01;
-    // xOff2 += 0.01;
+    xOff1 += 0.005
+    xOff2 += 0.005
 }
