92b1dd21db06c1e12c0d8c35dace5323cb408b99
Marvling <33942399+Marvling@users.noreply.github.com>
2020-05-22 13:21:35 -0700

    made noise added sliders to noise4

--- p5/noise1.js
+++ p5/noise1.js
@@ -7,8 +7,8 @@ let xOff2 = 100
 let xOff3 = 200
 
 let increment1 = 0.01
-let increment2 = 0.03
-let increment3 = 0.05
+let increment2 = 0.09
+let increment3 = 0.01
 
 function setup() {
     createCanvas(600, 600);
@@ -27,15 +27,30 @@ function draw() {
     xOff2 = offsetStart2;
     xOff3 = offsetStart3;
 
-    for(let y = 0; y < height; y++){
-        let x = (noise(xOff1)+noise(xOff2)+noise(xOff3))*(width/3)
-        vertex(x, y);
+    for(let y1 = 0; y1 < height; y1++){
+        let x1 = (noise(xOff1)+sin(xOff2)*0.05+noise(xOff3))*(width/2)
+        vertex(x1, y1);
+        
 
         xOff1 += increment1
         xOff2 += increment2
         xOff3 += increment3
 
     }
+    endShape();
+
+    beginShape();
+    for(let x2 = 0; x2 < height; x2++){
+        let y2 = (noise(xOff1)+noise(xOff2)+noise(xOff3))*(width/3)
+        vertex(x2, y2);
+        
+
+        xOff1 += increment1
+        xOff2 += increment2
+        xOff3 += increment3
+
+    }
+   
     endShape();
     // noLoop();
     offsetStart1 += increment1
--- p5/noise3.js
+++ p5/noise3.js
@@ -6,55 +6,46 @@ let xOff1 = 0
 let xOff2 = 100
 let xOff3 = 200
 
-let increment1 = 0.01
-let increment2 = 0.09
-let increment3 = 0.01
+let increment1 = 0.00001
+let increment2 = 0.00001
+let increment3 = 0.00001
 
 function setup() {
     createCanvas(600, 600);
-    // frameRate(1);
-
 }
 
 function draw() {
 
-    background(150);
-    stroke(0);
-    noFill();
+    loadPixels();
+    for(let x = 0; x < width; x++){
+        for (let y = 0; y < height; y++) {
 
-    beginShape();
+            let index = (x + y * width) * 4;
+
+            let n1 = noise(xOff1)*255;
+            let n2 = noise(xOff2)*255;
+            let n3 = noise(xOff3)*255;
+
+            xOff1 += increment1;
+            xOff2 += increment2;
+            xOff3 += increment3;
+
+            pixels[index + 0] = n1;
+            pixels[index + 1] = n2;
+            pixels[index + 2] = n3;
+            pixels[index + 3] = 255;
+            
+        }
+    }
+    updatePixels();
+
     xOff1 = offsetStart1;
     xOff2 = offsetStart2;
     xOff3 = offsetStart3;
 
-    for(let y1 = 0; y1 < height; y1++){
-        let x1 = (noise(xOff1)+sin(xOff2)*0.05+noise(xOff3))*(width/2)
-        vertex(x1, y1);
-        
-
-        xOff1 += increment1
-        xOff2 += increment2
-        xOff3 += increment3
-
-    }
-    endShape();
-
-    beginShape();
-    for(let x2 = 0; x2 < height; x2++){
-        let y2 = (noise(xOff1)+noise(xOff2)+noise(xOff3))*(width/3)
-        vertex(x2, y2);
-        
-
-        xOff1 += increment1
-        xOff2 += increment2
-        xOff3 += increment3
-
-    }
-   
-    endShape();
     // noLoop();
     offsetStart1 += increment1
-    // offsetStart2 += increment2
+    offsetStart2 += increment2
     offsetStart3 += increment3
    
 }
