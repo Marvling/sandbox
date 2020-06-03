4a7bbab75975d146115fca07e645108ced33e5e5
Marvling <33942399+Marvling@users.noreply.github.com>
2020-06-03 11:03:55 -0700

    mandal studies

--- p5/mandal2.js
+++ p5/mandal2.js
@@ -2,3 +2,30 @@ function setup() {
     var canvas = createCanvas(window.innerWidth, window.innerHeight)
 
 }
+
+function simetri(x, y) {
+
+    x += width / 2
+    y += height / 2
+
+    return [x, y]
+
+}
+
+function draw() {
+    stroke(10);
+    background(150);
+
+    beginShape()
+    vertex(mouseX, mouseY)
+    vertex(width / 2, height / 2)
+    endShape()
+
+    beginShape()
+    vertex(Math.abs(mouseX - width), Math.abs(mouseY - height))
+    vertex(width / 2, height / 2)
+    endShape()
+
+
+
+}
