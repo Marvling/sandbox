bd4c992b2559a7be95645f813876b3493b5d2a3b
Marvling <33942399+Marvling@users.noreply.github.com>
2020-06-05 13:02:50 -0700

    mandala finsihed

--- p5/mandal3.js
+++ p5/mandal3.js
@@ -1,24 +1,48 @@
-let linesNumber = 8;
+let linesNumber = 32;
 let angle;
+
+let strokeColor;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    angle = 2 * PI / linesNumber;
     background(150);
+
+    angle = 2 * PI / linesNumber;
+    strokeColor = color(255, 204, 0);
 }
 
 function mouseDragged() {
     translate(width / 2, height / 2);
     for (let i = 0; i < linesNumber; i++) {
-        rotate(-angle);
-        point(mouseX - width / 2, mouseY - height / 2);
-    }
+        rotate(angle);
+        if (i % 2 == 0) {
+            beginShape(LINES);
+            vertex(mouseX - width / 2, mouseY - height / 2);
+            vertex(pmouseX - width / 2, pmouseY - height / 2);
+            endShape();
+        } else {
+            beginShape(LINES);
+            vertex(-(mouseX - width / 2), mouseY - height / 2);
+            vertex(-(pmouseX - width / 2), pmouseY - height / 2);
+            endShape();
+        }
 
-    translate(width / 2, height / 2);
+    }
 }
 
 function draw() {
+
     noFill();
-    stroke(255);
+    stroke(strokeColor);
     strokeWeight(3)
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        strokeColor = color(255, 204, 0)
+    } else if (keyCode === RIGHT_ARROW) {
+        strokeColor = color(187, 246, 250);
+        console.log('anan');
+
+    }
 }
