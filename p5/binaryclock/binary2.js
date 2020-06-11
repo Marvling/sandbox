ae53a5eb0e00039935b9cc4bd985a5fd6c593088
Marvling <33942399+Marvling@users.noreply.github.com>
2020-06-10 18:33:13 -0700

    started working on binary clock

---
+++ p5/binaryclock/binary2.js
@@ -0,0 +1,2 @@
+// add int value to every circle
+// check the addition to fill the cicrcles
--- p5/test_timer/script.js
+++ p5/test_timer/script.js
@@ -9,18 +9,18 @@ function draw() {
 
     background(150);
     let h = nf(hour(), 2, 0);
-    let m = nf(minute(), 2,0);
+    let m = nf(minute(), 2, 0);
     let s = nf(second(), 2, 0);
 
     let time = `${h}:${m}:${s}`
 
     textAlign(CENTER, CENTER);
-    text(time, windowWidth/2, windowHeight/2)
+    text(time, windowWidth / 2, windowHeight / 2)
     textSize(100);
 
-    if ( hour() == 24 && timeUp == true) {
+    if (hour() == 24 && timeUp == true) {
         console.log('DOLDU HOCAM DOLDU!');
         timeUp = false
     }
-    
+
 }
